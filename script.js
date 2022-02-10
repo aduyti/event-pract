ec3b66a83c2acdd7f20ed05fdc17b6d563f76dff
Anirudhya <anirudhyaduyti@gmail.com>
2022-02-10 14:57:51 +0600

    so-so

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-document.getElementById("x");
+const inputBtn = document.getElementById("add-button");
+var count = 0;
+inputBtn.addEventListener("click", function () {
+    const inputText = document.getElementById("input-box");
+    if (inputText.value != "") {
+        count++;
+        const newRow = document.createElement("tr");
+        newRow.innerHTML = `
+            <td>${count}</td>
+            <td>${inputText.value}</td>
+            <td><button class="delete-btn btn btn-danger">Delete</button></td>`;
+        document.getElementById("table-content").appendChild(newRow);
+        inputText.value = "";
+
+        const detectButton = document.getElementsByClassName("delete-btn");
+        for (const btn of detectButton) {
+            // console.log(btn);
+            btn.addEventListener("click", function (event) {
+                // console.log(btn);
+                event.target.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode);
+                // console.log(event.target.parentNode.parentNode.parentNode);
+                // console.log(event.target.parentNode.parentNode);
+            })
+        }
+    }
+})
+
+document.getElementById("clear-all").addEventListener("click", function () {
+    document.getElementById("table-content").innerHTML = "";
+    count = 0;
+})
